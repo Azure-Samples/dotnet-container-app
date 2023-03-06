b36739c4b635234fa7a96a85e464e55e56f6b415
Leandro Prado <leadro@microsoft.com>
2023-03-06 17:47:01 -0300

    add blob storage

---
+++ src/ContainerApp.WeatherApi/Data/BlobStorage.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ContainerApp.WeatherApi.Data
+{
+    public class BlobStorage
+    {
+        private string blobConnection = "DefaultEndpointsProtocol=https;AccountName=testeernestogaia;AccountKey=vckJ5MFD+VvmSwHRXX+9WBNDC0RQ8W6YdJerkJb1YfIcVbJtNEvkLjIXmRDbJ6EQA2uVeOv1Lax7+AStVFgNuQ==;EndpointSuffix=core.windows.net";
+
+        public BlobStorage()
+        {
+        }
+
+        public string Upload()
+        {
+            return blobConnection;
+        }
+    }
+}
